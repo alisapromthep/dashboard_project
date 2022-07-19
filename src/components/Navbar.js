06956b8d71197b76efb4c17adb7195ee10882ba6
Alisa Promthep <alisa.promthep@gmail.com>
2022-07-18 23:07:57 -0400

    navbar and sidebar open and close mechanism

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { AiOutlineMenu } from 'react-icons/ai';
 import { FiShoppingCart } from 'react-icons/fi';
 import { BsChatLeft } from 'react-icons/bs';
@@ -10,6 +10,7 @@ import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const NavButton = ( {title, customFunc, icon, color, dotColor} )=>(
+
   <TooltipComponent content={title}
   position="ButtomCenter">
     <button type="button" onClick={customFunc}
@@ -20,15 +21,31 @@ const NavButton = ( {title, customFunc, icon, color, dotColor} )=>(
       >
         {icon}
       </span>
-
-
     </button>
-
   </TooltipComponent>
 )
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu, handleClick} = useStateContext();
+  const { activeMenu, setActiveMenu, handleClick, isClicked, setIsClicked, screenSize, setScreenSize } = useStateContext();
+
+  useEffect(()=>{
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+    //without removing, it would check everytime it loads 
+    return () => window.removeEventListener('resize', handleResize);
+
+  }, [])
+
+  useEffect(()=>{
+    if(screenSize <= 900 ){
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  },[screenSize]);
 
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
@@ -74,6 +91,10 @@ const Navbar = () => {
           </div>
 
         </TooltipComponent>
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
       </div>
 
 
