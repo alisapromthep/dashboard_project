b329b6082db149b0b15d3a67fb2ccf3fb6a516e3
Alisa Promthep <alisa.promthep@gmail.com>
2022-07-20 10:23:20 -0400

    fixed slidebar links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -44,7 +44,7 @@ const Sidebar = () => {
               </p>
               {item.links.map((link)=>(
                 <NavLink
-                to={`/$(link.name)`}
+                to={`/${link.name}`}
                 key={link.name}
                 onClick={handleCloseSideBar}
                 className={({ isActive }) => (isActive ? activeLink:normalLink)}>
